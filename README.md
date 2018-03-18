2bddceba8dc380055d41da32566a32d2667d6630
Mike AUBENAS <mike.aubenas@gmail.com>
2018-03-18 22:43:38 +0100

    Solved the problem with babel, mocha, npm ... everything [...] #5 #1

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ The JSON file containing all the notes from octave 0 to 8 has been parsed by mys
 You will also find some example of unit tests with the [MochaJS framework](https://mochajs.org/)
 
 # Use
+
+# What I learnt
+
+- To launch a JavaScript project simply do `npm init` on the folder, I can also do `npm init -y` if a project already exists. A file named package.json will appear, it's like a build.gradle in Android : It manages the dependences of a project
+- Babel is transcompiling my ES6 code into ES5 so that the brawser can interpret it
+- npm is more than a package manager, I can create my very own commands to launch scripts juste like my `npm run test`
--- core/NoteFactory.js
+++ core/NoteFactory.js
@@ -1,10 +1,8 @@
-class NoteFactory
+export default class NoteFactory
 {
-    constructor(){}
-
-    setListNotes()
+    constructor()
     {
-        const arrayOfFrequencies =
+        this._arrayOfFrequencies = 
         [
             27.500, 29.135, 30.868, 32.703, 34.648, 36.708, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55.000, 58.270, 
             61.735, 65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.83, 110.00, 116.54, 123.47, 130.81,
@@ -15,9 +13,66 @@ class NoteFactory
             3520.0, 3729.3, 3951.1, 4186.0
         ];
 
-        const arrayOfPeriodsMs = 
+        this._arroyOfPeriods =
         [
-            
-        ]; /* TODO */
+            36.36, 34.32, 32.40, 30.58, 28.86, 27.24, 25.71, 24.27, 22.91, 21.62, 20.41, 19.26, 18.18, 17.16, 16.20, 15.29,
+            14.29, 13.62, 12.86, 12.13, 11.45, 10.81, 10.20, 9.631, 9.091, 8.581, 8.099, 7.645, 7.216, 6.811, 6.428, 6.068,
+            5.272, 5.405, 5.102, 4.816, 4.545, 4.290, 4.050, 3.822, 3.608, 3.405, 3.214, 3.034, 2.863, 2.703, 2.551, 2.408,
+            2.273, 2.145, 2.025, 1.190, 1.804, 1.703, 1.607, 1.517, 1.432, 1.351, 1.276, 1.204, 1.136, 1.073, 1.012, 0.9556,
+            0.9020, 0.8513, 0.8034, 0.7584, 0.7159, 0.6757, 0.6378, 0.6020, 0.5682, 0.5363, 0.5062, 0.4778, 0.4510, 0.4257,
+            0.4018, 0.3792, 0.3580, 0.3378, 0.3189, 0.3010, 0.2841, 0.2681, 0.2531, 0.2389
+        ];
+
+        this._arrayOfNotes = this.generateArrayOfNotes();
+    }
+
+    getArrayOfFrequencies()
+    {
+        return this._arrayOfFrequencies;
+    }
+
+    getArrayOfPeriods()
+    {
+        return this._arroyOfPeriods;
+    }
+
+    generateArrayOfNotes()
+    {
+        let arrayNotes = new Array();
+        let firstOctave = [ ["A", 0], ["A#", 0], ["B", 0], ["C", 1], ["C#", 1], 
+            ["D", 1], ["D#", 1], ["E", 1], ["F", 1], ["F#", 1], ["G", 1], ["G#", 1] ];
+
+        for(let i = 0; i < 7; i++)
+        {
+            for(let j = 0; j < firstOctave.length; j++)
+            {
+                let currentNote = firstOctave[j];
+                
+                let newNote = new Array(currentNote[0], currentNote[1] + i);
+
+                arrayNotes.push(newNote[0] + newNote[1].toString());
+            }
+        }
+
+        function addLastNotes(array)
+        {
+            array.push("A7", "A#7", "B7", "C8", "C#8");
+        }
+
+        addLastNotes(arrayNotes);
+
+        return arrayNotes;
+        /* 
+            ! TODO correct, length not natching others arrays
+        */
+    }
+
+    getArrayOfNotes()
+    {
+        return this._arrayOfNotes;
     }
 }
+
+let factory = new NoteFactory();
+
+console.log(factory.getArrayOfNotes());
